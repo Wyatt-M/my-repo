cd8a3d828e760458a0bd4d3f8031f5ad0e0ff360
Wyatt-M <wmattas@stumail.fhtc.edu>
2016-08-26 09:27:25 -0500

    Commit

--- TestingGit/src/Program.java
+++ TestingGit/src/Program.java
@@ -1,9 +1,13 @@
 
 public class Program {
 
+	public Scanner in = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+		
+		in.close();
 	}
 
 }
