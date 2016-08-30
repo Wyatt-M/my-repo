9f9fc49c1c739b867c4c5cb8c05393ecbb9a6d0b
Wyatt-M <wmattas@stumail.fhtc.edu>
2016-08-30 10:00:51 -0500

    Track.java homework

---
+++ Track/src/Prompt.java
@@ -0,0 +1,25 @@
+
+public class Prompt {
+
+	private int trackNum;
+	public int getTrackNum() { return trackNum; }
+	public void setTrackNum(int tn) { trackNum = tn; }
+	
+	private String title;
+	public String getTitle() { return title; }
+	public void setTitle(String t) { title = t; }
+	
+	private String artist;
+	public String getArtist() { return artist; }
+	public void setArtist(String a) { artist = a; }
+	
+	private int duration;
+	public int getDuration() { return duration; }
+	public void setDuration(int d) { duration =d; }
+
+	private int bitRate;
+	public int getBitRate() { return bitRate; }
+	public void setBitRate(int b) { bitRate = b; }
+	
+	
+}
---
+++ Track/src/Track.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Track {
+
+	public static Scanner in = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+	Prompt p1 = new Prompt();
+
+	
+	
+	in.close();
+	}
+
+}
